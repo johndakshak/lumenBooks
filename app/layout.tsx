d27f5ac502372e2ba6dcef386d5c16e0fec5cbe7
johndakshak <johndakshak@gmail.com>
2026-06-21 10:24:41 +0100

    fix: Remove Navbar and Footer omponents from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,10 +29,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <Navbar />
-        <main className="flex-1 pt-20">{children}</main>
-        <Footer />
+      <body className="">
+        {children}
       </body>
     </html>
   );
